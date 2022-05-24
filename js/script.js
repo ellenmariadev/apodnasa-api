49fc323c5dac60982a4d3bd309051a0c375b6e2c
ellenmariadev <ellenmariadev@gmail.com>
2022-05-24 14:27:32 -0300

    styling details + final JS

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
-$("#date").attr('max', new Date().toISOString().split('T')[0])
+$("#date").attr("max", new Date().toISOString().split("T")[0])
+$("#date").attr("min", new Date("1995-06-16").toISOString().split("T")[0])
 
-// https://api.nasa.gov/planetary/apod?api_key=Lz4FMIyYzMnu6k72C479hHA2DkfJDeaHNKjw7g74
-//Lz4FMIyYzMnu6k72C479hHA2DkfJDeaHNKjw7g74
+$(document).ready(function() {
+    $("#btn").click(function() {
+        getApi();
+    })
+})
 
-$("#btn").on('click', function() {
+ function getApi() {
     
     var dt = $("#date").val();
     
@@ -16,21 +20,18 @@ $("#btn").on('click', function() {
             var mediaUrl = response.url
             var mediaType = response.media_type
 
-            $('#name').html(name)
-            $('#date').html(date)
-            $('#explanation').html(`<span>EXPLANATION: </span>` +explanation)
-            $('h4').html(date)
+            $("#name").html(name)
+            $("#date").html(date)
+            $("#explanation").html(`<span>EXPLANATION: </span>` + explanation)
+            $("h4").html(date)
 
-            
-
-            if (mediaType === 'video') {
-                $('.media').html(`<iframe width="560" height="315" src="${mediaUrl}" frameborder="0" allow="accelerometer; allowfullscreen=""></iframe>`)
+            if (mediaType === "video") {
+                $(".media").html(`<iframe width="560" height="315" src="${mediaUrl}" frameborder="0"  allowfullscreen=""></iframe>`)
             } else {
-                $('.media').html(`<img src="${mediaUrl}">`)
+                $(".media").html(`<img src="${mediaUrl}">`)
             }
-
-            console.log(nameImg)
-            console.log(mediaType)
         }
     })
-})
+}
+
+getApi();
